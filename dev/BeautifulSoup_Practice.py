dfcdf9adf35d1bc94c3c2adacba4acd8aff49f86
Sam L <lafell.samuel@gmail.com>
2022-08-13 12:38:36 -0500

    updates

--- dev/BeautifulSoup_Practice.py
+++ dev/BeautifulSoup_Practice.py
@@ -12,7 +12,11 @@ import re
 from datetime import date, datetime
 from time import strptime
 import numpy as np
-url = 'https://www.baseball-reference.com/boxes/?month=4&day=7&year=2022'
+import os
+import pandas as pd
+import sys
+sys.path.append('dev/')
+import functions as f
 
 #tables = my_object.find_all('table', class='teams')
 #tables
@@ -24,9 +28,28 @@ url = 'https://www.baseball-reference.com/boxes/?month=4&day=7&year=2022'
 ################################################################
 ##################### Grab all the Base URLs ###################
 ################################################################
+
+
+x = f.Game_Ids(2022)
+
+x.find_file()
+x.schedule_html()
+x.game_ids_to_scape()
+
+x.find_file()
+x.file_exists()
+
+x.game_ids_to_scape
+
+schedule_html = x.schedule_html()
+print(schedule_html)
+
+
 schedule_url = 'https://www.baseball-reference.com/leagues/majors/2022-schedule.shtml'
 schedule_request = requests.get(schedule_url)
 schedule_html = BeautifulSoup(schedule_request.text, 'html.parser')
+######### Total Games in Season #############
+total_games = len(schedule_html.find_all(class_="game"))
 
 ######### if before today, grab the score, if not, leave score blank #############
 score_pattern = re.compile("(\d)")
@@ -34,12 +57,15 @@ score_pattern = re.compile("(\d)")
 ######### All Game Days in Season #############
 game_days = [day.text for day in schedule_html.find_all('h3')]
 
-######### Total Games in Season #############
-total_games = len(schedule_html.find_all(class_="game"))
+
 ######### Total Games in Season #############
 game_list = []
 
-for game_num in range(total_games-1):
+
+indexes_needed = game_ids_to_scape(2022)
+
+
+for game_num in indexes_needed:
     ############ Day of Game Played ############
     game_day = schedule_html.find_all(class_="game")[game_num].parent.h3.text
     if game_day == "Today's Games":
@@ -70,19 +96,37 @@ for game_num in range(total_games-1):
         home_team = schedule_html.find_all(class_="game")[game_num].contents[5].text.strip()
         home_team_score = np.NaN
         
+    if game_num %% 100 == 0:
+        print(f'Game Number: {game_num} of {total_games} games.')
     game_list.append([date_string_to_append, away_team, home_team, away_team_score, home_team_score])
     
-import pandas as pd
+    
 games2022 = pd.DataFrame(game_list, columns=['Date', 'Away_Team', 'Home_Team', 'Away_Score', 'Home_Score'])
-games2022.to_csv('/Users/samlafell/Documents/Learning/Sports_Betting_Project/Data/Intermediate/games2022.csv')
+games2022.to_csv('data/interim/games2022.csv')
 
 
+year = 2022
+if games_file_path:
+    games = pd.read_csv(games_file_path)
+    game_ids_to_scrape = games[games['Away_Score'].isnull()].index
+    
+    
 
 #################################################################################
 ##################### Grab all the boxscore URLs for that day ###################
 #################################################################################
+
+# Load in CSV with all game days
 games2022 = pd.read_csv('/Users/samlafell/Documents/Learning/Sports_Betting_Project/Data/Intermediate/games2022.csv')
+
+# Format URL
+game_days = games2022.iloc[:,0]
+opening_day = game_days[0]
+opening_day.date()
 url = 'https://www.baseball-reference.com/boxes/?month=4&day=7&year=2022'
+
+
+
 my_result = requests.get(url)
 my_object = BeautifulSoup(my_result.text, 'html.parser')
 # Search the Webpage for all the box scores available for the day and append them to a list
---
+++ dev/functions.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import pandas as pd
+import numpy as np
+
+
+class Game_Ids():
+    def __init__(self, year):
+        self.year = year
+        self.file_location = f'/Users/samlafell/Documents/Learning/sports_betting_project/data/interim/games{self.year}.csv'
+        
+    def find_file(self):
+        self.file_exists = os.path.exists(self.file_location)
+    
+    def schedule_html(self):
+        schedule_url = f'https://www.baseball-reference.com/leagues/majors/{self.year}-schedule.shtml'
+        schedule_request = requests.get(schedule_url)
+        return BeautifulSoup(schedule_request.text, 'html.parser')
+
+    def game_ids_to_scape(self):
+        if self.find_file():
+            # Just the games that are left to fill in
+            games = pd.read_csv(self.file_location)
+            return games[games['Away_Score'].isnull()].index.values
+        else:
+            ######### Total Games in Season #############
+            return np.array([i for i in range(len(self.schedule_html.find_all(class_="game")))])
+        
+
